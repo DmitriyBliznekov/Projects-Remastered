0e3306021107119136f69325d51726dcac849234
Dmitriy Bliznekov <BliznekovDmitriy@gmail.com>
2017-11-17 13:16:37 +0300

    Added enumeration of user's actions

---
+++ Project/Project/Helpers/UserActionsMainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Helpers
+{
+    public enum UserActionsMainWindow
+    {
+        None,
+        AddNew,
+        Delete,
+        ChangeExist,
+        Load,
+        Save,
+        Clear
+    }
+}
